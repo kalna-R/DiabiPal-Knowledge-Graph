2ffd7dbbb27dd4aef592a57fda49e9fbe0e1d772
kalnar328 <40544324+kalnar328@users.noreply.github.com>
2020-10-20 10:19:47 +0530

    app.py modified

--- app.py
+++ app.py
@@ -6,20 +6,19 @@ from correction import post_process
 
 # from autocorrect import Speller
 
-app = Flask(__name__)
+import spacy
+from spacy.matcher import Matcher
 
-# import spacy
-# from spacy.matcher import Matcher
-#
-# nlp = spacy.load("en_core_web_sm")
-# matcher = Matcher(nlp.vocab)
+nlp = spacy.load("en_core_web_sm")
+matcher = Matcher(nlp.vocab)
+app = Flask(__name__)
 
 
 @app.route('/hey')
 def process_array():
     ocr_array = '{FBS]'
 
-    only_string = " ".join(re.findall("[a-zA-Z]+", ocr_array))
+    only_string = " ".join(re.findall('[a-zA-Z]+', ocr_array))
     print(only_string)
 
     return ocr_array
@@ -31,9 +30,9 @@ def process_array():
 #     return spell('{fbs]')
 
 
-
 @app.route('/corrections', methods=["POST"])
 def post_processing():
+    # skip the content requirement by force=True
     data = request.get_json(force=True)
 
     corrected_array = post_process(data)
